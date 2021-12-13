95b88d784cf22653ca7f7b6aa4ae5293ffb4142f
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-13 21:36:08 +0100

    structure

---
+++ pages/authors/me.js
@@ -0,0 +1,3 @@
+export default function MeAuthors() {
+    return <h1 > This is Authors ME page in Next.js < /h1>
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+    return ( <
+        >
+        <
+        h1 > First Post Next.js < /h1> <
+        h2 >
+        <
+        Link href = "/" >
+        <
+        a > Back to home < /a> <
+        /Link> <
+        /h2>
+
+        <
+        />
+
+
+    )
+
+
+
+}
